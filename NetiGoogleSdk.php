b9493ce36be97fe95f6367d725b8660752a00f0e
Hendrik Rombach <hr@netinventors.de>
2017-06-16 14:39:33 +0200

    Actually commit the damn code. (derp)

--- NetiGoogleSdk.php
+++ NetiGoogleSdk.php
@@ -8,8 +8,18 @@
 namespace NetiGoogleSdk;
 
 use Shopware\Components\Plugin;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class NetiGoogleSdk extends Plugin
 {
+    const MINIMUM_SDK_VERSION  = "2.0.0";
+    const BREAKING_SDK_VERSION = "2.1.0";
+
+    public function build(ContainerBuilder $container)
+    {
+        $container->setParameter('neti_google_sdk.vendor_dir', $this->getPath() . '/vendor');
+
+        parent::build($container);
+    }
 
 }
---
+++ Service/GoogleSdkFactory.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @copyright  Copyright (c) 2017, Net Inventors GmbH
+ * @category   Shopware
+ * @author     hrombach
+ */
+
+namespace NetiGoogleSdk\Service;
+
+use NetiGoogleSdk\Exceptions\GoogleSdkFactoryException;
+use NetiGoogleSdk\NetiGoogleSdk;
+
+class GoogleSdkFactory
+{
+    /**
+     * @var \Google_Client[]
+     */
+    private static $clients;
+
+    /**
+     * @var string
+     */
+    private $vendorPath;
+
+    /**
+     * SdkFactory constructor.
+     *
+     * @param string $vendorPath
+     */
+    public function __construct($vendorPath)
+    {
+        $this->vendorPath = $vendorPath;
+    }
+
+    /**
+     * @param string      $applicationName
+     * @param string      $developerKey
+     * @param array       $config
+     * @param string|null $cacheKey
+     *
+     * @return \Google_Client
+     * @throws \NetiGoogleSdk\Exceptions\GoogleSdkFactoryException
+     */
+    public function getClient($applicationName, $developerKey, array $config = [], $cacheKey = null)
+    {
+        $this->loadLibrary();
+
+        $config += [
+            'application_name' => $applicationName,
+            'developer_key'    => $developerKey,
+        ];
+
+        $cacheKey = (null === $cacheKey) ? $developerKey : $cacheKey;
+
+        $client = self::$clients[$cacheKey];
+
+        if (!$client instanceof \Google_Client) {
+            $client = self::$clients[$cacheKey] = new \Google_Client($config);
+        }
+
+        return $client;
+    }
+
+    /**
+     * @return bool
+     * @throws GoogleSdkFactoryException
+     */
+    private function loadLibrary()
+    {
+        if (!class_exists('Google_Client')) {
+            require_once $this->vendorPath . '/autoload.php';
+        } else {
+            if (!$this->assertCompatible()) {
+                throw new GoogleSdkFactoryException(
+                    sprintf(
+                        'Loaded Google Client version "%s" is incompatible with required version "%s".',
+                        \Google_Client::LIBVER, NetiGoogleSdk::MINIMUM_SDK_VERSION
+                    )
+                );
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    private function assertCompatible()
+    {
+        return (
+            version_compare(\Google_Client::LIBVER, NetiGoogleSdk::MINIMUM_SDK_VERSION, '>=') &&
+            version_compare(\Google_Client::LIBVER, NetiGoogleSdk::BREAKING_SDK_VERSION, '<=')
+        );
+    }
+}
